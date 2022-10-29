ace5a269c1702e21e032f3e595b862670118d1e1
eliseusbrito <eliseusbrito@gmail.com>
2022-10-29 10:33:45 -0300

    Associado autor ao livro via id

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -3,9 +3,11 @@ import livros from "../models/Livro.js"
 class LivroController {
 
     static listarLivros = (req, res) => {
-        livros.find((err, livros) => {
-            res.status(200).json(livros)
-        })
+        livros.find()
+            .populate('autor')
+            .exec((err, livros) => {
+                res.status(200).json(livros)
+            })
     }
 
     static listarLivroPorId = (req, res) => {
--- src/models/Livro.js
+++ src/models/Livro.js
@@ -1,10 +1,11 @@
+import mongoose from "mongoose"
 import mongosse from "mongoose"
 
 const livrosSchema = new mongosse.Schema(
     {
         id: { type: String },
         titulo: { type: String, required: true },
-        autor: { type: String, required: true },
+        autor: { type: mongoose.Schema.Types.ObjectId, ref: 'autores', required: true },
         editora: { type: String, required: true },
         numeroPaginas: { type: Number }
     }
